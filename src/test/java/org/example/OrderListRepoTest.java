afe66e46e769ee5c19ed8d21a0643779c8ccd963
WeiMing <110397919+WeiMingTay@users.noreply.github.com>
2023-09-11 23:31:57 +0200

    Order und OrderlistRepo geschrieben

---
+++ src/test/java/org/example/OrderListRepoTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderListRepoTest {
+
+    @Test
+    void getOrder() {
+    }
+
+    @Test
+    void getOrderById() {
+    }
+
+    @Test
+    void addOrder() {
+    }
+
+    @Test
+    void removeOrder() {
+    }
+}
