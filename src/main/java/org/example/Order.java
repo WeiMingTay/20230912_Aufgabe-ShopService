1a7eda017127b6c090aaa04a151cb392212b7434
WeiMing <110397919+WeiMingTay@users.noreply.github.com>
2023-09-12 10:54:13 +0200

    BEstellstatus der Order hinzugefügt(enum)

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -4,6 +4,13 @@ import java.util.List;
 
 public record Order(
         String id,
-        List<Product> products
+        List<Product> products,
+        OrderStatus status
 ) {
+    public enum OrderStatus {
+        PROCESSING,
+        IN_DELIVERY,
+        COMPLETED
+    }
 }
+
